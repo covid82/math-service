7307d37a5221480180de4f0e2324e664092c9ecb
deniszjukow <denis.zjukow@gmail.com>
2020-05-03 20:38:17 +0200

    Added pull-build-publish.sh

--- pull-build-publish.sh
+++ pull-build-publish.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 git pull
+set SBT_OPTS="-Xms512M -Xmx1024M -Xss2M -XX:MaxMetaspaceSize=1024M" sbt
+sbt assembly
 docker build -t math-service .
 docker tag math-service:latest 489683348645.dkr.ecr.eu-west-1.amazonaws.com/math-service:latest
 docker push 489683348645.dkr.ecr.eu-west-1.amazonaws.com/math-service:latest
